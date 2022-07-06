3cc3158e9de27f640868823a484e931cd7b3096a
Jared <jared@virtanatech.com>
2022-07-06 14:44:12 -0400

    Shader method updated,extra shaders for robot and marker removed+ code formatting + shader in script found via resources.load method instead of in inspector + sharpie file moved to models folder + images folders checked first for presence and created if not already existing

---
+++ RobotTesting/Assets/Resources/Shader/WhiteReplacementShader.shader
@@ -0,0 +1,38 @@
+Shader "Unlit/WhiteReplacementShader" {
+  Properties {
+    _MainTex ("Texture", 2D) = "white" {}
+  }
+  
+  SubShader {
+    Tags { "RenderType"="Opaque" } //Subshader with same tag "MyTag"
+    
+    Pass {
+      CGPROGRAM
+      #pragma vertex vert
+      #pragma fragment frag
+        
+        
+      #pragma shader_feature WORLD_NORMAL_VIS
+      #include "UnityCG.cginc"
+      
+      struct appdata{
+        float4 vertex : POSITION;
+      };
+      
+      struct v2f {
+        float4 vertex : SV_POSITION;
+      };
+      
+      v2f vert (appdata v) {
+        v2f o;
+        o.vertex = UnityObjectToClipPos(v.vertex);
+        return o;
+      }
+      
+      fixed4 frag () : SV_Target {
+        return fixed4(1,1,1,1);  //Whire colour of marker
+      }
+      ENDCG
+    }
+  }
+}
--- RobotTesting/Assets/Scripts/CameraController.cs
+++ RobotTesting/Assets/Scripts/CameraController.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 public class CameraController : MonoBehaviour{
 
   public bool MaskOn;
-  public Shader ReplacShaderWhite;
   
   private int _imageNumber;	
   private Texture2D _camView;
   private Camera _overHeadCam;
+  private string _filePath;
 
-  void GetCamView(){
+  void GetCamView() {
     RenderTexture currentRenText = RenderTexture.active;
     RenderTexture.active = _overHeadCam.targetTexture;
 
@@ -27,27 +27,44 @@ public class CameraController : MonoBehaviour{
     byte[] bytes = _camView.EncodeToJPG();
 
     //If the tick box is selected in the inspector, then the path is changed accordingly
-    if(MaskOn) {
-      File.WriteAllBytes( "MaskedImages/MaskedImage" + _imageNumber + ".jpg" , bytes );
-    } else {
-      File.WriteAllBytes( "Images/Image" + _imageNumber + ".jpg" , bytes );
-    }
+
+    File.WriteAllBytes( _filePath + _imageNumber + ".jpg" , bytes );
     
     _imageNumber++; 
   }
 
   private void Start(){
     _overHeadCam = GetComponent<Camera>();
+    Shader replacementShader = Resources.Load<Shader>("Shader/WhiteReplacementShader");
      
     //If the tick box is selected in the inspector, the replacement shader is loaded for cam
-    if (MaskOn)
-    {
-      _overHeadCam.SetReplacementShader(ReplacShaderWhite, "RenderQueue");
-    } 
+    if (MaskOn) {
+      _filePath = "MaskedImages/MaskedImage";
+      _overHeadCam.SetReplacementShader( replacementShader , "RenderType");
+
+      InitilaiseDirectories();
+      //The directories need to be checked first, does not matter if it checked in the masked
+      //script or regular, just only once , ie not twice across the two scripts of the camera
+
+    } else {
+      _filePath = "Images/Image";
+    }
+
     _camView = new Texture2D(_overHeadCam.targetTexture.width ,_overHeadCam.targetTexture.height);
 		
     //Starting the repeating function that takes pictures
     InvokeRepeating("GetCamView",1.5f,3.0f);   
   }
 
+  void InitilaiseDirectories(){ 
+    if (!Directory.Exists("MaskedImages")) {
+      Directory.CreateDirectory("MaskedImages");
+    }
+    
+
+    if (!Directory.Exists("Images")) {
+      Directory.CreateDirectory("Images");
+    }
+    //creates the directory if it doesnt exist
+  }
 }
